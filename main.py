a7dbbc93f5a9937d0cc84f028aba6b9773d4cd5a
Michell <mgoris18@gmail.com>
2025-01-02 21:49:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import json
+import os
 
 from collections import OrderedDict
 
+def load_tasks():
+    if not os.path.exists('tasks.json'):
+        with open('tasks.json', 'w') as file:
+            json.dump([], file)
+        return []
+    
+    with open('tasks.json', 'r') as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return []
+
+def save_tasks(tasks):
+    with open('tasks.json', 'w') as file:
+        json.dump(tasks, file, indent=4)
+
+
+def get_next_id(tasks):
+    return max([task['id'] for task in tasks], default=0) + 1 
 #request task from user
 
 task = input("Add Task: ")
