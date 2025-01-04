19990c4452c12f594d4fb84a454510ff33cadb2b
Michell <mgoris18@gmail.com>
2025-01-03 21:23:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,33 +22,23 @@ def save_tasks(tasks):
 
 def get_next_id(tasks):
     return max([task['id'] for task in tasks], default=0) + 1 
-#request task from user
 
-task = input("Add Task: ")
-tasks = []
-tasks.append(task)
+def load_tasks():
+    if not os.path.exists('tasks.json'):
+        with open('tasks.json', 'w') as file:
+            json.dump([], file)
+        return []
+    
+    with open('tasks.json', 'r') as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return []
 
-while True:
-    a = input("Add Tasks? [Y/N]")
-    if a == "y":
-        print(input("Add Task: "))
-        tasks.append
-    elif a == "n":
-        print(tasks)
+def save_tasks(tasks):
+    with open('tasks.json', 'w') as file:
+        json.dump(tasks, file, indent=4)
 
 
-#res = [{val: key for key, val in enumerate(
-    #OrderedDict.fromkeys(tasks))}
-    #[ele] for ele in tasks]
-
-#json_object = json.dumps(tasks)
-
-#with open("tasks.json", "w") as outfile:
-    #outfile.write(json_object)
-
-#print("Task added succesfully. ID:" + str(res))
-#while True:
-   # a = input("Add More Tasks? [Y/N]")
-    #if a == "y":
-        #print()
-
+def get_next_id(tasks):
+    return max([task['id'] for task in tasks], default=0) + 1
